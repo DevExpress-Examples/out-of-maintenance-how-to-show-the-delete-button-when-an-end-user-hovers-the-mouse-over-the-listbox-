5614271cb144ed4bb099d78fb05831f22a5bf0ce
DevExpressExampleBot <fakeEmail>
2019-04-11 20:48:05 +0300

    Source auto update [skip ci]

---
+++ VB/DeleteItemFromListBox/Form1.vb
@@ -0,0 +1,22 @@
+﻿Imports DevExpress.XtraEditors
+
+Namespace DeleteItemFromListBox
+	Partial Public Class Form1
+		Inherits XtraForm
+
+		Public Sub New()
+			InitializeComponent()
+
+			For i As Integer = 0 To 9
+				listBoxControl1.Items.Add("Click the context button to delete this record " & i)
+			Next i
+
+			AddHandler listBoxControl1.ContextButtonClick, AddressOf ListBoxControl1_ContextButtonClick
+		End Sub
+
+		Private Sub ListBoxControl1_ContextButtonClick(ByVal sender As Object, ByVal e As DevExpress.Utils.ContextItemClickEventArgs)
+			Dim listBoxControl = TryCast(sender, ListBoxControl)
+			listBoxControl.Items.Remove(e.DataItem)
+		End Sub
+	End Class
+End Namespace
---
+++ VB/DeleteItemFromListBox/Program.vb
@@ -0,0 +1,19 @@
+﻿Imports System
+Imports System.Collections.Generic
+Imports System.Linq
+Imports System.Windows.Forms
+
+Namespace DeleteItemFromListBox
+	Friend Module Program
+		''' <summary>
+		''' The main entry point for the application.
+		''' </summary>
+		<STAThread>
+		Sub Main()
+			DevExpress.LookAndFeel.UserLookAndFeel.Default.SetSkinStyle(DevExpress.LookAndFeel.SkinStyle.Bezier)
+			Application.EnableVisualStyles()
+			Application.SetCompatibleTextRenderingDefault(False)
+			Application.Run(New Form1())
+		End Sub
+	End Module
+End Namespace
