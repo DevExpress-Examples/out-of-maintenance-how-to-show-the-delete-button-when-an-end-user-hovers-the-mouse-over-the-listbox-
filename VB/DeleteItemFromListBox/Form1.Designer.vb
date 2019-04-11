ccb2181fe7de566f4cdff8958adca7ac1ec33312
DevExpressExampleBot <fakeEmail>
2019-04-11 20:44:02 +0300

    Source auto update [skip ci]

---
+++ VB/DeleteItemFromListBox/Form1.Designer.vb
@@ -0,0 +1,58 @@
+﻿Namespace DeleteItemFromListBox
+	Partial Public Class Form1
+		''' <summary>
+		''' Required designer variable.
+		''' </summary>
+		Private components As System.ComponentModel.IContainer = Nothing
+
+		''' <summary>
+		''' Clean up any resources being used.
+		''' </summary>
+		''' <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+			If disposing AndAlso (components IsNot Nothing) Then
+				components.Dispose()
+			End If
+			MyBase.Dispose(disposing)
+		End Sub
+
+		#Region "Windows Form Designer generated code"
+
+		''' <summary>
+		''' Required method for Designer support - do not modify
+		''' the contents of this method with the code editor.
+		''' </summary>
+		Private Sub InitializeComponent()
+			Me.listBoxControl1 = New DevExpress.XtraEditors.ListBoxControl()
+			CType(Me.listBoxControl1, System.ComponentModel.ISupportInitialize).BeginInit()
+			Me.SuspendLayout()
+			' 
+			' listBoxControl1
+			' 
+			Me.listBoxControl1.Dock = System.Windows.Forms.DockStyle.Fill
+			Me.listBoxControl1.HotTrackItems = True
+			Me.listBoxControl1.Items.AddRange(New Object() { "0", "1", "2", "3", "4", "5", "6", "7", "8", "9"})
+			Me.listBoxControl1.Location = New System.Drawing.Point(0, 0)
+			Me.listBoxControl1.Name = "listBoxControl1"
+			Me.listBoxControl1.Size = New System.Drawing.Size(284, 262)
+			Me.listBoxControl1.TabIndex = 0
+			' 
+			' Form1
+			' 
+			Me.AutoScaleDimensions = New System.Drawing.SizeF(6F, 13F)
+			Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+			Me.ClientSize = New System.Drawing.Size(284, 262)
+			Me.Controls.Add(Me.listBoxControl1)
+			Me.Name = "Form1"
+			Me.Text = "Form1"
+			CType(Me.listBoxControl1, System.ComponentModel.ISupportInitialize).EndInit()
+			Me.ResumeLayout(False)
+
+		End Sub
+
+		#End Region
+
+		Private listBoxControl1 As DevExpress.XtraEditors.ListBoxControl
+	End Class
+End Namespace
+
---
+++ VB/DeleteItemFromListBox/Form1.vb
@@ -0,0 +1,15 @@
+﻿Imports System
+Imports System.Collections.Generic
+Imports System.Linq
+Imports System.Windows.Forms
+
+Namespace DeleteItemFromListBox
+	Partial Public Class Form1
+		Inherits Form
+
+		Public Sub New()
+			InitializeComponent()
+			Dim tempVar As New DeleteHelper(listBoxControl1)
+		End Sub
+	End Class
+End Namespace
